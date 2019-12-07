b503fe6640db281e53b9ac91dfc4138dc0c14f19
elenaneo <57110448+elenaneo@users.noreply.github.com>
2019-12-07 08:44:14 +0500

    Update Task32.java

--- Task32.java
+++ Task32.java
@@ -1,33 +1,19 @@
-public class Task32{
- 
-   public static void main(String args[]){
-	   String myString = "sdfdsf,srwe14,7854,,dgd,afrfg6789421,afasd234,1235zcpoyudnvode..,10";
-	   char myChar='f';
-	 
-	   System.out.println(mFind(myString,myChar)); 
-   }
-	   static Integer mFind(String mSource,char mFindch){
-		Integer iFind =0, lastFind = mSource.lastIndexOf(mFindch), firstFind = mSource.indexOf(mFindch);
-		System.out.println("firstFind-"+firstFind);
-		System.out.println("lastFind-"+lastFind);
-		if (firstFind ==-1)
-			return iFind;
-		else if (lastFind==firstFind)
-			return 1;
-		else 
-		{ iFind=iFind+1;
-		do
-		{
-			iFind=iFind+1;
-			mSource=mSource.substring(firstFind+1);
-			firstFind = mSource.indexOf(mFindch);
-			System.out.println("firstFind-"+firstFind);
-		}
-		while (lastFind!=firstFind);
-		return iFind;	
-		}
-	   }	
-   }
+public class Task6 {
+        public static void main(String[] args) {
+            System.out.println(union_str("abgsgdc","ckkelswa"));
+        }
+
+        public static String union_str(String str1, String str2) {
+            String rez_str = new String ();
+            for (char s: (str1+str2).toCharArray()) {
+                if (rez_str.indexOf(s) == -1) {
+                    rez_str += s;
+                }
+            }
+            return rez_str;
+        }
+
+    }
 
    
    
